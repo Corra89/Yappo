1a4e0124918d154f359abfd60ce4f8439bfe162f
Corra89 <marco.corrarati@engimtorino.net>
2023-05-10 15:12:51 +0200

    1

---
+++ JAVASCRIPT/lezione1.js
@@ -0,0 +1,30 @@
+let a ='ciao';
+let b = 2+3;
+console.log(a);
+console.log(b);
+const base = 10;
+const altezza = 5;
+let area = base*altezza;
+console.log(area);
+console.log("La tua Area è" + (base*altezza) + "cm");
+let num=5;
+console.log(num % 2);
+
+console.log(base==altezza);
+
+console.log(base % 2 == 0);
+
+console.log(altezza % 10 == 0);
+
+console.log((altezza % 2 == 0)&&(base % 2 == 0));
+
+let anno=2023
+
+console.log((anno % 4 == 0)&&(anno % 100 != 0)||(anno % 400 == 0)) 
+
+
+
+
+
+//alert("Ciao!"); PopUp
+
