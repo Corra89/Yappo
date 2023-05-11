0aae7d78318ac79e2e8b718ff2bd3794b3fc2810
Corra89 <corraratimarco@gmail.com>
2023-05-11 14:27:07 +0200

    ok

--- JAVASCRIPT/lezione2.js
+++ JAVASCRIPT/lezione2.js
@@ -61,14 +61,22 @@ for(numero of array){
     if(numero % 2 == 0)
     console.log(numero);
 }
-*/
- 
-let i = 0
-let array1 = [1,2,3];
-let array2 = [4,5,6];
-let risultato = []
-for(let i = 0; i < array1.length; i++){ 
-console.log
-}
 
+let i = 2
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let risultato = [];
+for (let i = 0; i < array1.length; i++) {
+    risultato.push(array1[i] + array2[i]);
+    console.log(risultato[Number]);
+}
+*/
 //array1.push(4) istruzione per aggiungere un elemento alla fine di una lista
+
+const array1 = ["Paolo","Fabio","Giovanni","Gabriele","Francesco","Andrea","Marco","Federico","Renato"];
+console.log(array1);
+const array2 = [];
+for (let i = 0; i < array1.length; i++){
+    array1[i].startsWith("F") && array2.push(array1[i]);
+}
+console.log(array2);
