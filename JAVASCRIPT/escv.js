4814f3c16f4cbc6f32c75cdff175dc7cc40896cc
Corra89 <marco.corrarati@engimtorino.net>
2023-06-08 17:27:20 +0200

    ok

--- JAVASCRIPT/escv.js
+++ JAVASCRIPT/escv.js
@@ -1,26 +1,41 @@
- // Array di esperienze lavorative
+const apiKey = "appid=dbb76c5d98d5dbafcb94441c6a10236e&";
+const latitude = 45.07;
+const longitude = 7.69;
+
+const currentWeatherDiv = document.getElementById("current-weather");
+
+fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&appid=${apiKey}`)
+.then(response => response.json())
+.then(data => {
+    const currentWeather = data.current_weather;
+    currentWeatherDiv.innerHTML = `Temperatura corrente: ${currentWeather.temperature}°C`;
+ })
+.catch(error => console.error(error));
+ 
  const esperienzeLavorative = [
     {
-      azienda: "Azienda 1",
+      azienda: "WAYNE ENTERPRISES",
       ruolo: "Responsabile",
-      dataInizio: "01/01/2020",
+      luogo: "Gotham",
+      dataInizio: "04/05/1993",
       dataFine: "01/06/2020"
     },
     {
-      azienda: "Azienda 2",
-      ruolo: "Programmatore",
+      azienda: "JUSTICE LEAGUE MEMBER",
+      ruolo: "Socio part-time",
+      luogo: "Gotham",
       dataInizio: "01/07/2020",
       dataFine: "01/12/2020"
     },
     {
-      azienda: "Azienda 3",
-      ruolo: "Responsabile",
-      dataInizio: "01/12/2020",
-      dataFine: "Presente"
+      azienda: "COMMUNITY INVOLVEMENT",
+      ruolo: "Vigilante",
+      luogo: "Gotham",
+      dataInizio: "20/06/2000",
+      dataFine: "08/06/2023"
     }
   ];
 
-  // Popola le card delle esperienze lavorative
   const esperienzeLavorativeContainer = document.getElementById("esperienze-lavorative");
   esperienzeLavorative.forEach(esperienza => {
     const card = document.createElement("div");
@@ -29,23 +44,25 @@
       <div class="card-body">
         <h5 class="card-title">${esperienza.azienda}</h5>
         <p class="card-text">${esperienza.ruolo}</p>
+        <p class="card-text">${esperienza.luogo}</p>
         <p class="card-text">Dal ${esperienza.dataInizio} al ${esperienza.dataFine}</p>
       </div>
     `;
     esperienzeLavorativeContainer.appendChild(card);
   });
 
-  // Gestisce l'aggiunta di una nuova esperienza lavorativa
   const formEsperienzaLavorativa = document.getElementById("form-esperienza-lavorativa");
   formEsperienzaLavorativa.addEventListener("submit", event => {
     event.preventDefault();
     const azienda = document.getElementById("azienda").value;
     const ruolo = document.getElementById("ruolo").value;
+    const luogo = document.getElementById("luogo").value;
     const dataInizio = document.getElementById("data-inizio").value;
     const dataFine = document.getElementById("data-fine").value;
     const nuovaEsperienza = {
       azienda: azienda,
       ruolo: ruolo,
+      luogo: luogo,
       dataInizio: dataInizio,
       dataFine: dataFine
     };
@@ -56,12 +73,10 @@
       <div class="card-body">
         <h5 class="card-title">${nuovaEsperienza.azienda}</h5>
         <p class="card-text">${nuovaEsperienza.ruolo}</p>
+        <p class="card-text">${nuovaEsperienza.luogo}</p>
         <p class="card-text">Dal ${nuovaEsperienza.dataInizio} al ${nuovaEsperienza.dataFine}</p>
       </div>
     `;
     esperienzeLavorativeContainer.appendChild(card);
     formEsperienzaLavorativa.reset();
   });
-
-
-//newExperience.innerHTML = `${"Anno di Inizio: " + annoInizio.value} ${"Anno di Fine: " + annoFine.value} ${"Nome del Lavoro: " + nomeLavoro.value} ${"Nome dell'azienda: " + nomeAzienda.value}`;
